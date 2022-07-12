584f841f1129109620ac20c3439051dfcfd7acba
Kirabo19 <kapojames@gmail.com>
2022-07-12 04:28:40 +0300

    Add linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,26 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: "./src/index.html",
-    filename: "./index.html"
+  template: './src/index.html',
+  filename: './index.html',
 });
 
 module.exports = {
 
-    mode: 'development',
-    module: {
-        rules: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+      },
     },
-    plugins: [htmlPlugin]
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    },
+    ],
+  },
+  plugins: [htmlPlugin],
 };
